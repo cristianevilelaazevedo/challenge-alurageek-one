bc4631c22d9076af1eac962fc1199063fc935920
cristianevilelaazevedo <chrystianevilela27@gmail.com>
2024-06-04 15:00:53 -0300

    first commit

--- js/deletarProdutos.js
+++ js/deletarProdutos.js
@@ -1,11 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
 
-// async function deletarProdutos(ProdutoId) {
-//     await conectaApi.deletarProdutos(ProdutoId);
-// }
-
-// export {deletarProdutos};
-
 export async function deletarProdutos(id){
     const conexao = await fetch(`http://localhost:3000/products/${id}`, {
         method: 'DELETE'
