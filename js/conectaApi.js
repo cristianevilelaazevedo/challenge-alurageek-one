7d3f723771958bbb5dc6e492e748504db99415ec
cristianevilelaazevedo <chrystianevilela27@gmail.com>
2024-06-05 15:38:36 -0300

    adicionando link da api

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,14 +1,14 @@
 //Link da api no vercel = https://json-server-api-seven.vercel.app/
 
 async function listaProdutos() {
-    const conexao = await fetch("https://json-server-api-seven.vercel.app/produtos");
+    const conexao = await fetch("https://json-server-api-seven.vercel.app/products");
     const conexaoConvertida = await conexao.json();
     
     return conexaoConvertida;
 }
 
 async function criaProdutos(nome, valor, imagem) {
-    const conexao = await fetch("https://json-server-api-seven.vercel.app/produtos", {
+    const conexao = await fetch("https://json-server-api-seven.vercel.app/products", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
@@ -30,7 +30,7 @@ async function criaProdutos(nome, valor, imagem) {
 
 async function deletarProdutos(ProdutoId) {
     try {
-        const conexao = await fetch(`https://json-server-api-seven.vercel.app/produtos/${ProdutoId}`, {
+        const conexao = await fetch(`https://json-server-api-seven.vercel.app/products/${ProdutoId}`, {
             method: "DELETE",
         });
 
